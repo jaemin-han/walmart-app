d7d12fa3e153bbbda3eefbf0232be64d1a214d48
Alexander Tong <tongsalex@gmail.com>
2016-12-07 12:51:48 -0500

    add chart input and button

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
-
 import './Header.css';
 import '../reset.css';
+import walmart from './walmart.jpg';
 
 const Header = props => (
   <div id="header-container">
-   <link href="https://fonts.googleapis.com/css?family=Megrim" rel="stylesheet" />
-    <div id="header-title">collektiv</div>
+    {<img src={walmart} alt="walmart" id="walmart-logo" />}
   </div>
     );
 
