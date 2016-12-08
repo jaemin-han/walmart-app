727c49f9ad957fae852f9a0e692f886313f4e2f3
Jaemin Han <jaemin.han@nyu.edu>
2016-12-08 12:05:28 -0500

    update to date

---
+++ src/components/Data_Chart/Data_Chart.jsx
@@ -0,0 +1,43 @@
+  // import React from 'react';
+  // import './Data_Chart.css';
+  // import '../reset.css';
+  // import rd3 from 'react-d3';
+
+  // const lineData = [
+  //   {
+  //     name: "series1",
+  //     values: [ { x: 0, y: 20 }, { x: 24, y: 10 } ],
+  //     strokeWidth: 3,
+  //     strokeDashArray: "5,5",
+  //   },
+  //   ....
+  //   {
+  //     name: "series2",
+  //     values: [ { x: 70, y: 82 }, { x: 76, y: 82 } ]
+  //   }
+  // ];
+
+
+  // const Data_Chart = props => (
+  //   <div id="data-chart">
+  //     <link href="https://fonts.googleapis.com/css?family=Inconsolata" rel="stylesheet" />
+  //     <LineChart
+  //       legend={true}
+  //       data={lineData}
+  //       width='100%'
+  //       height={400}
+  //       viewBoxObject={
+  //         x: 0,
+  //         y: 0,
+  //         width: 500,
+  //         height: 400
+  //       }
+  //       title="Line Chart"
+  //       yAxisLabel="Altitude"
+  //       xAxisLabel="Elapsed Time (sec)"
+  //       gridHorizontal={true}
+  //     />
+  //   </div>
+  //     );
+
+  // export default Data_Chart;
