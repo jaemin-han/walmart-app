27c684780bc7d0ed00e88bb9af2003b2aeac576a
Alexander Tong <tongsalex@gmail.com>
2016-12-08 10:13:35 -0500

    unsure

--- src/components/Chart_Container/Chart_Container.jsx
+++ src/components/Chart_Container/Chart_Container.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './Chart_Container.css';
 import '../reset.css';
+var LineChart = require("react-chartjs").Line;
 
 const Chart_Container = props => (
   <div id="chart-container">
     <link href="https://fonts.googleapis.com/css?family=Inconsolata" rel="stylesheet" />
     <input type="text" id="chart-input" />
+    {/*<LineChart data={chartData} options={chartOptions} width="600" height="250" />*/}
     <button id="chart-input-button">Enter</button>
   </div>
     );
