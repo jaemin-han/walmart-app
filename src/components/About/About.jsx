331d8587b9693740f5994bfb9c2f1c318e21183a
Jaemin Han <jaemin.han@nyu.edu>
2016-12-08 09:59:25 -0500

    walmart website

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -5,15 +5,34 @@ const About = React.createClass({
   render: function() {
     return(
       <div className="about-container">
-
+      <link href="https://fonts.googleapis.com/css?family=Nunito+Sans" rel="stylesheet" />
         <h2 id="about_h2">About</h2>
-        <p id="about_p">Lorem Ipsum is simply dummy text of the printing and typesetting
-        industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
-        when an unknown printer took a galley of type and scrambled it to make a type specimen book.
-        It has survived not only five centuries, but also the leap into electronic typesetting, remaining
-        essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing
-        Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including
-        versions of Lorem Ipsum.</p>
+        <p id="about_p">You are provided with historical sales data for 45 Walmart stores
+        located in different regions. Each store contains a number of departments,
+        and you are tasked with predicting the department-wide sales for each store.
+        <br />
+        <br />
+        *This is the historical training data, which covers to 2010-02-05 to 2012-11-01.
+        <br />
+        *Within this file you will find the following fields
+        <br />
+        *Store - the store number
+        <br />
+        *Dept - the department number
+        <br />
+        *Date - the week
+        <br />
+        *Weekly_Sales -  sales for the given department in the given store
+        <br />
+        *IsHoliday - whether the week is a special holiday week
+        <br />
+        <br />
+        In addition, Walmart runs several promotional markdown events throughout the year.
+        These markdowns precede prominent holidays, the four largest of which are the Super Bowl,
+        Labor Day, Thanksgiving, and Christmas. The weeks including these holidays are weighted
+        five times higher in the evaluation than non-holiday weeks. Part of the challenge presented by
+        this competition is modeling the effects of markdowns on these holiday weeks in the absence of
+        complete/ideal historical data.</p>
       </div>
     )
   }
